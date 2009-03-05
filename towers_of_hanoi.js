6355fdce73e4934c2b6d75fcad29c629e4909b07
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-03-05 02:24:29 +0000

    Quashed bug in which old game painted on canvas following victory, rendering new game invisible.

--- towers_of_hanoi.js
+++ towers_of_hanoi.js
@@ -34,7 +34,8 @@ function Game() {
 Game.prototype.start_new = function() {
   debug.msg('New game');
 
-  var ctx = document.getElementById('canvas').getContext('2d');
+  this.recreate_canvas();
+  var ctx = this.canvas.getContext('2d');
   var tower_manager = new TowerManager(ctx);
   var input_handler = new InputHandler(ctx, tower_manager);
   var game_state = new GameState(tower_manager, input_handler);
@@ -42,6 +43,16 @@ Game.prototype.start_new = function() {
   game_state.on_victory = function() { victory_celebrator.on_victory(); }
 
   tower_manager.draw();
+}
+
+// Necessary to quash bug in which redraw of old game state occurs after victory, redrawing old game state over new,
+// rendering the new game invisible until player forces redraw by clicking on disk. For details, see comment beginning
+// "All right, prepare for things to get downright wacky" in InputHandler.prototype.on_canvas_mouseup.
+Game.prototype.recreate_canvas = function() {
+  this.canvas = document.getElementById('canvas');
+  var canvas_prime = this.canvas.cloneNode(false);
+  this.canvas.parentNode.replaceChild(canvas_prime, this.canvas);
+  this.canvas = canvas_prime;
 }
 
 
@@ -69,11 +80,11 @@ function InputHandler(ctx, tower_manager) {
   this.tower_manager = tower_manager;
   this.canvas = ctx.canvas;
   this.coordinate_finder = new ElementCoordinateFinder(this.canvas);
-  this.add_event_handlers();
+  this.add_event_listeners();
   this.enable_input();
 }
 
-InputHandler.prototype.add_event_handlers = function() {
+InputHandler.prototype.add_event_listeners = function() {
   debug.msg('Adding event listeners');
   // Must use 'self', for when event handler is called, 'this' will refer not to the InputHandler instance I expect,
   // but to the element on which the event occurred -- in this case, the canvas element.
@@ -112,6 +123,34 @@ InputHandler.prototype.on_canvas_mouseup = function(event) {
   this.dragging = false;
   var closest_tower = this.tower_manager.find_closest_tower(this.disk.centre);
   this.disk.transfer_to_tower(closest_tower);
+  // All right, prepare for things to get downright wacky. The line below is at the root of a difficult-to-find
+  // bug in which, after the game is won and a new one is started, the canvas is redrawn with the old disks in the
+  // position that won the game, rendering the new disks invisible. The flow of the application is as follows:
+  //
+  //   * The line above (this.disk.transfer_to_tower) causes GameState's on_disk_transferred callback to be called.
+  //   * When the game is won, GameState's on_disk_transferred in turn calls VictoryCelebrator's on_victory callback.
+  //   * VictoryCelebrator's on_victory creates a new game. Everything happens as one would expect -- new towers and
+  //     associated disks are created, and the canvas is redrawn to show the new towers & disks.
+  //   * When Game's constructor exits, having initialized the new game, the call stack unwinds. Execution returns to
+  //     the end of VictoryCelebrator.on_victory, then the end of GameState.on_disk_transferred, then the end of
+  //     Disk.transfer_to_tower, then the end of InputHandler.on_canvas_mouseup -- that's here! With the call above to
+  //     Disk.transfer_to_tower having completed, the line below is executed.
+  //
+  // The problem is that this.tower_manager refers to the *old* TowerManager, with the old towers and the old disks,
+  // but the same canvas element as is used in the new game. Thus, the canvas is overwritten with the old towers and
+  // disks. To see the proper game state, the player must then click on the invisible new disk located on the first
+  // tower -- doing so will force a redraw, showing the proper game state.
+  //
+  // I see no obvious way to correct this at this location, for the call to this.tower_manager.draw must come after the
+  // disk being dragged by the player has been moved to its new tower. Instead, in Game's initialization code, I shall
+  // recreate the canvas element. The post-victory redraw responsible for the bug shall then draw to a canvas element no
+  // longer within the document (but still valid, for it remains in memory -- garbage collection will destroy it in
+  // time).
+  //
+  // Incidentally, Firebug's Javascript debugger was invaluable in tracking the source of this issue -- without Firebug,
+  // I doubt I'd have figured it out. For whatever reason, with Firebug 1.3.3, the debugger wouldn't work when the
+  // document was accessed locally (through file://..., not through a Web server). Accessing the document on a remote
+  // server allowed Firebug's Javascript debugger to work, however.
   this.tower_manager.draw();
 }
 
@@ -289,8 +328,9 @@ function VictoryCelebrator(input_handler) {
 }
 
 VictoryCelebrator.prototype.on_victory = function() {
-  debug.msg('Victory!');
+  debug.msg('<strong>Victory!</strong>');
   this.input_handler.disable_input();
+  new Game();
 }
 
 
@@ -298,7 +338,6 @@ VictoryCelebrator.prototype.on_victory = function() {
 // Tower
 //=======
 // TODO: refactor to use Point class throughout.
-// TODO: remove index parameter in constructor and from class
 function Tower(x, y, ctx) {
   this.x = x;
   this.y = y;
