9930ee9d9ebde55009c0a3638c286c93318e9d47
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-03-19 12:45:31 +0000

    Added introductory message explaining the game.

--- javascripts/init.js
+++ javascripts/init.js
@@ -1,5 +1,8 @@
 function init() {
   debug = new Debug(); // TODO: convert to singleton to eliminate global variable.
-  new Game(4);
+  new Game(2);
+  document.getElementById('start-new-game').addEventListener('click', function() {
+    document.getElementById('introduction').style.display = 'none';
+  }, false);
 }
 window.addEventListener('load', init, false);
