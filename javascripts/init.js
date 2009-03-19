67d17443a6729b4ff230fb7b6cd7c5569ad212df
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-03-19 13:27:26 +0000

    Forced sans-serif font family, for browsers use serif by default.

--- javascripts/init.js
+++ javascripts/init.js
@@ -1,6 +1,6 @@
 function init() {
   debug = new Debug(); // TODO: convert to singleton to eliminate global variable.
-  new Game(2);
+  new Game(3);
   document.getElementById('start-new-game').addEventListener('click', function() {
     document.getElementById('introduction').style.display = 'none';
   }, false);
